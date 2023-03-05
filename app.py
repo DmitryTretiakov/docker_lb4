545d27412bf3ca41b69140034fab6015569fb8bb
DmitryTretiakov <97670912+DmitryTretiakov@users.noreply.github.com>
2023-03-05 17:39:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, world!"
 
-if name == "main":
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
